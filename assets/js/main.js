17add1a994836f7c41ce5bf22b492f91385cdad9
Mariana Souza <marianacaldassouza@gmail.com>
2018-08-06 22:39:39 -0700

    Adding modes easy and hard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
+//var numOfSquares keep tracking of the number of squares that will be generated on hard(6) and easy (3) mode
+var numOfSquares = 6;
+
 //var colors is an array that contains all colors generated dynamically by the function generateRandomColors
-var colors = generateRandomColors(6);
+var colors = generateRandomColors(numOfSquares);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickRandomColor();
@@ -10,12 +13,15 @@ colorDisplay.textContent = pickedColor;
 var header = document.querySelector("header");
 
 var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easy");
+var hardButton = document.querySelector("#hard");
 
 reset.addEventListener("click" , function(){
 	//reseting background header colors
 	header.style.backgroundColor = "#232323";
+
 	//generating all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numOfSquares);
 
 	//it picks a new random color from array colors
 	 pickedColor = pickRandomColor();
@@ -27,8 +33,64 @@ reset.addEventListener("click" , function(){
 	 for(var i = 0; i < squares.length; i++){
 	 	squares[i].style.backgroundColor = colors[i];
 	 }
+});
+
+easy.addEventListener("click" , function(){
+	easyButton.classList.add("selected");
+	hardButton.classList.remove("selected");
+
+ 	//reseting background header colors
+	header.style.backgroundColor = "#232323";
+
+ 	//generating 3 new colors
+ 	numOfSquares = 3;
+	colors = generateRandomColors(numOfSquares);
+
+	//it picks a new random color from array colors
+	 pickedColor = pickRandomColor();
+
+	//change color displayed to match picked color
+	colorDisplay.textContent = pickedColor;
+
+	//change colors of the squares
+	 for(var i = 0; i < squares.length; i++){
+	 	//Untill we get to the third square
+	 	if(colors[i]){
+	 	 squares[i].style.backgroundColor = colors[i];
+	 	}else{
+	 		//from the fourth square, we hide them
+	 		squares[i].style.display = "none";
+	 	}
+	 	
+	 }
 
 });
+
+hard.addEventListener("click" , function(){
+	hardButton.classList.add("selected");
+	easyButton.classList.remove("selected");
+
+	//reseting background header colors
+	header.style.backgroundColor = "#232323";
+
+ 	//generating 6 new colors
+ 	numOfSquares = 6;
+	colors = generateRandomColors(numOfSquares);
+
+	//it picks a new random color from array colors
+	 pickedColor = pickRandomColor();
+
+	//change color displayed to match picked color
+	colorDisplay.textContent = pickedColor;
+
+	//change colors of the squares
+	 for(var i = 0; i < squares.length; i++){
+	 	squares[i].style.backgroundColor = colors[i];
+	 	squares[i].style.display = "block";
+	 	
+	 }
+});
+
 
 //Message that will be displayed to guide the user
 var messageDisplay = document.querySelector("#message");
